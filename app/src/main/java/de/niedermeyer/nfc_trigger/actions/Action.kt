8d25130360eec93d354024077ab9e27a9841f391
Milan Höllner <milan.hoellner@gmail.com>
2019-01-18 23:49:31 +0100

    make Action/AlarmAction a Parcelable to prevent exception (#14)

--- app/src/main/java/de/niedermeyer/nfc_trigger/actions/Action.kt
+++ app/src/main/java/de/niedermeyer/nfc_trigger/actions/Action.kt
@@ -1,13 +1,24 @@
 package de.niedermeyer.nfc_trigger.actions
 
+import android.os.Parcel
+import android.os.Parcelable
 import kotlinx.serialization.Serializable
 
 @Serializable
-abstract class Action {
+abstract class Action : Parcelable {
 
     abstract var TYPE: Int
 
     abstract var VAL: Array<Int>
 
     abstract fun doAction()
+
+    override fun describeContents(): Int {
+        return hashCode()
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(TYPE)
+        dest?.writeArray(VAL)
+    }
 }
--- app/src/main/java/de/niedermeyer/nfc_trigger/actions/alarm/AlarmAction.kt
+++ app/src/main/java/de/niedermeyer/nfc_trigger/actions/alarm/AlarmAction.kt
@@ -2,27 +2,60 @@ package de.niedermeyer.nfc_trigger.actions.alarm
 
 import android.content.Context
 import android.content.Intent
+import android.os.Parcel
+import android.os.Parcelable
 import android.provider.AlarmClock
 import de.niedermeyer.nfc_trigger.R
 import de.niedermeyer.nfc_trigger.actions.Action
 import de.niedermeyer.nfc_trigger.actions.ActionConstants
-import kotlinx.serialization.json.JSON
-import kotlinx.serialization.stringify
-import org.json.JSONArray
 
-class AlarmAction(val context: Context, var hours: Int, var minutes: Int) : Action() {
-    override var TYPE = ActionConstants.ALARM
+class AlarmAction() : Action() {
+    override var TYPE: Int = ActionConstants.ALARM
+    override var VAL: Array<Int> = arrayOf()
+    var context: Context? = null
 
-    override var VAL = arrayOf(hours, minutes)
+    var hours: Int
+        get() = VAL[0]
+        set(value) {
+            VAL[0] = value
+        }
+    var minutes: Int
+        get() = VAL[1]
+        set(value) {
+            VAL[1] = value
+        }
 
     override fun doAction() {
         val intent = Intent(AlarmClock.ACTION_SET_ALARM)
-                .putExtra(AlarmClock.EXTRA_HOUR, hours)
-                .putExtra(AlarmClock.EXTRA_MINUTES, minutes)
-        context.startActivity(intent)
+                .putExtra(AlarmClock.EXTRA_HOUR, VAL[0])
+                .putExtra(AlarmClock.EXTRA_MINUTES, VAL[1])
+        context!!.startActivity(intent)
     }
 
     override fun toString(): String {
-        return context.getString(R.string.action_alarm_tostring, hours, minutes)
+        return context!!.getString(R.string.action_alarm_tostring, VAL[0], VAL[1])
+    }
+
+    constructor(context: Context, hours: Int, minutes: Int) : this() {
+        this.context = context
+        VAL = arrayOf(hours, minutes)
+    }
+
+    private constructor (src: Parcel?) : this() {
+        if (src is Parcel){
+            TYPE = src.readInt()
+            VAL = src.createIntArray().toTypedArray()
+        }
+    }
+
+    companion object CREATOR: Parcelable.Creator<AlarmAction>{
+        override fun newArray(size: Int): Array<AlarmAction?> {
+            return arrayOfNulls(size)
+        }
+
+        override fun createFromParcel(source: Parcel?): AlarmAction {
+            return AlarmAction(source)
+        }
+
     }
 }
